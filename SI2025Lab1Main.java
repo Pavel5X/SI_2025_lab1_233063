35deb0b52927f4d8c52e4a38497f2ea35cd36ef5
PAVEL 5 <pavel.petkovski@students.finki.ukim.mk>
2025-04-12 17:53:08 +0200

    Added missing ; sign

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -65,7 +65,7 @@ class TaskManager {
     // 1. Remove a task by name
     public void removeTask(String name) {
         // TODO: Implement removal logic
-        tasks.removeIf(task -> task.getName().equalsIgnoreCase(name))
+        tasks.removeIf(task -> task.getName().equalsIgnoreCase(name));
     }
 
     // 2. Find all completed tasks
